7418e1fbb94aae05e6511d3f87e09e0414a1b771
Kevin Herrera <kevin@herrera.io>
2021-07-12 22:40:08 +0000

    Defining path to configuration directory.

--- dev/__init__.py
+++ dev/__init__.py
@@ -1,4 +1,5 @@
-from os import path
+from os import environ, path
+from pathlib import Path
 from typing import Final
 
 __all__ = ["cli", "command"]
@@ -9,3 +10,12 @@ DOCKER_DIR: Final[Path] = path.normpath(path.join(
     "..",
     "docker"
 ))
+
+# The path to the configuration directory.
+CONFIG_DIR: Final[Path] = Path(path.normpath(path.join(
+    environ.get("XDG_CONFIG_HOME") or path.join(Path.home(), ".config"),
+    "dev"
+)))
+
+if not CONFIG_DIR.exists():
+    CONFIG_DIR.mkdir(mode=0o755, parents = True)
