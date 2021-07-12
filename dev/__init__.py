8db8cd2a3fb44eefea2069a11e3a8df29acf2328
Kevin Herrera <kevin@herrera.io>
2021-07-12 22:39:41 +0000

    Fixing type.

--- dev/__init__.py
+++ dev/__init__.py
@@ -4,7 +4,7 @@ from typing import Final
 __all__ = ["cli", "command"]
 
 # The path to the build directory.
-DOCKER_DIR: Final = path.normpath(path.join(
+DOCKER_DIR: Final[Path] = path.normpath(path.join(
     path.dirname(__file__),
     "..",
     "docker"
