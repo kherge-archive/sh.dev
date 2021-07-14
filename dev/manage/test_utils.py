793b720a9ce965864baeba4442331da3469bdede
Kevin Herrera <kevin@herrera.io>
2021-07-14 02:46:12 +0000

    Adding user and group utilities.

--- dev/manage/test_utils.py
+++ dev/manage/test_utils.py
@@ -2,9 +2,55 @@ from . import utils
 
 from unittest import mock
 
+@mock.patch("os.getgid")
+def test_get_group_id(mock_getgid: mock.Mock):
+    mock_getgid.return_value = 123
+
+    result = utils.get_group_id()
+
+    mock_getgid.assert_called_once()
+
+    assert result == 123
+
+@mock.patch("grp.getgrgid")
+@mock.patch("os.getgid")
+def test_get_group_name(mock_getgid: mock.Mock, mock_getgrgid: mock.Mock):
+    mock_getgid.return_value = 123
+    mock_getgrgid.return_value.gr_name = "test"
+
+    result = utils.get_group_name()
+
+    mock_getgid.assert_called_once()
+    mock_getgrgid.assert_called_once_with(123)
+
+    assert result == "test"
+
 @mock.patch("dev.manage.config.get")
-def test_get_label(mock_get):
+def test_get_label(mock_get: mock.Mock):
     mock_get.return_value = "value"
 
     assert utils.get_label() == f"{utils.LABEL_NAME}=value"
     assert utils.get_label(with_name=False) == "value"
+
+@mock.patch("os.getuid")
+def test_get_user_id(mock_getuid: mock.Mock):
+    mock_getuid.return_value = 123
+
+    result = utils.get_user_id()
+
+    mock_getuid.assert_called_once()
+
+    assert result == 123
+
+@mock.patch("pwd.getpwuid")
+@mock.patch("os.getuid")
+def test_get_user_name(mock_getuid: mock.Mock, mock_getpwuid: mock.Mock):
+    mock_getuid.return_value = 123
+    mock_getpwuid.return_value.pw_name = "test"
+
+    result = utils.get_user_name()
+
+    mock_getuid.assert_called_once()
+    mock_getpwuid.assert_called_once_with(123)
+
+    assert result == "test"
--- dev/manage/utils.py
+++ dev/manage/utils.py
@@ -1,10 +1,29 @@
 from . import config
-
 from typing import Final
+
+import grp
+import os
+import pwd
 
 # The name of the label used to segregate workspaces.
 LABEL_NAME: Final[str] = "io.github.kherge.sh-dev.workspace"
 
+def get_group_id():
+    """Returns the user's group ID."""
+    return os.getgid()
+
+def get_group_name():
+    """Returns the user's group name."""
+    return grp.getgrgid(os.getgid()).gr_name
+
 def get_label(with_name=True):
     """Returns the label used for all managed objects."""
     return (f"{LABEL_NAME}=" if with_name else "") + config.get("core.label")
+
+def get_user_id():
+    """Returns the user's ID."""
+    return os.getuid()
+
+def get_user_name():
+    """Returns the user's name."""
+    return pwd.getpwuid(os.getuid()).pw_name
